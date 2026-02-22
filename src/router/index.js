069d8b181c2e836e121c2044ff34c1cdf633c4e6
LIeyuan621 <1163954848@qq.com>
2026-02-22 21:29:57 +0800

    Feat : 安卓适配

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
     { path: '/', redirect: '/timeline' },
-    { path: '/timeline', name: 'TimelineEditor', component: () => import('../views/TimelineEditor.vue') },
+    { path: '/timeline', name: 'Timeline', component: () => import('../views/TimelineEntry.vue') },
     { path: '/editor', name: 'DataEditor', component: () => import('../views/DataEditor.vue') }
 ]
 
